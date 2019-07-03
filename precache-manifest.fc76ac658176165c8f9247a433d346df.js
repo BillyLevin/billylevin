23a9eddf45780f9c46d0d287bb6768f5983fd2a2
Billy Levin <billylevs@gmail.com>
2019-07-03 14:52:08 +0100

    Updates

---
+++ precache-manifest.fc76ac658176165c8f9247a433d346df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3404ae38d8dab193400d5b9ec5f7ff6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "76462179312a6285f5ea",
+    "url": "/portfolio/static/js/2.554d8e8a.chunk.js"
+  },
+  {
+    "revision": "4a7f150833077d764f33",
+    "url": "/portfolio/static/js/main.b3e6ed2f.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4808fb778e9a75c7ee8dfbe76e5c07bf",
+    "url": "/portfolio/static/media/booklist-thumbnail.4808fb77.png"
+  },
+  {
+    "revision": "1eee788ca6b1ce4d71f0f12acfcaad6e",
+    "url": "/portfolio/static/media/cvBillyLevin.1eee788c.pdf"
+  },
+  {
+    "revision": "0d61d724cb0c14dea6214bf6ce37eb1e",
+    "url": "/portfolio/static/media/gymtracker-thumbnail.0d61d724.png"
+  },
+  {
+    "revision": "27b5070dd15f4ade172e20c04c3dc9a9",
+    "url": "/portfolio/static/media/hero.27b5070d.jpg"
+  },
+  {
+    "revision": "aa1f3b6e0881ffa0a8777cb59af17858",
+    "url": "/portfolio/static/media/mealplanner-thumbnail.aa1f3b6e.png"
+  },
+  {
+    "revision": "0ed21b800c2c67e89a262b0bffc18d00",
+    "url": "/portfolio/static/media/one-menu-thumbnail.0ed21b80.png"
+  }
+]);
